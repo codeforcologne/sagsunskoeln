ad174882fb1a68051b6857d6dd6ad61c1e35074e
Wolfram Eberius <eberius@gmail.com>
2016-03-07 14:01:19 +0100

    Update app.js
    add dialog

--- src/main/webapp/assets/js/app.js
+++ src/main/webapp/assets/js/app.js
@@ -12,4 +12,33 @@ $(document).ready(function() {
 			"data" : "updatedDatetime"
 		} ]
 	});
+	$('#data tbody').on('click', 'tr', function() {
+		if ($(this).hasClass('selected')) {
+			var data = table.row( this ).data();
+			$(this).removeClass('selected');
+			$("#markt").empty();
+			$("#links").empty();
+			$("#modaltitle").empty();
+		} else {
+			$("#markt").empty();
+			$("#links").empty();
+			$("#modaltitle").empty();
+			table.$('tr.selected').removeClass('selected');
+			$(this).addClass('selected');
+			$("#aboutModal").modal("show");
+			$("#modaltitle").append(table.row( this ).data().id);
+			$("#markt")
+				.append("<h3>" + table.row( this ).data().serviceName + "</h3>")
+				.append("<div>" + table.row( this ).data().description + "</div>")
+				.append("<h3>Adresse</h3>")
+				.append("<div>" + table.row( this ).data().address.split(',').join('</br>') + "</div>")
+				.append("<h3>Datum</h3>")
+				.append("<table>")
+				.append("<tr><td style='font-weight: bold;'>Anfrage:&nbsp;</td><td>" + table.row( this ).data().requestedDatetime + "</td></tr>")
+				.append("<tr><td style='font-weight: bold;'>Update:&nbsp;</td><td>" + table.row( this ).data().updatedDatetime + "</td></tr>")
+				.append("</table>")
+				;
+		}
+	});
+	
 });
